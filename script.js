3a6ec85354f55553c557d8fc7dc56e0db793cba4
Sahadeb Mandal <sdmrc19@gmail.com>
2024-03-12 22:16:16 +0530

    add functionality of laps

--- script.js
+++ script.js
@@ -81,6 +81,100 @@ stop_btn.addEventListener("click", ()=>{
 
 
 
+// add functionality on reset button
+let reset_btn = document.getElementById("reset-btn");
+reset_btn.addEventListener("click", ()=>{
+    clearInterval(setIntervalFunc);
+    hr = 0;
+    min = 0;
+    sec = 0;
+    ms = 0;
+    document.getElementById("hour").innerText = "00";
+    document.getElementById("minute").innerText = "00";
+    document.getElementById("second").innerText = "00";
+    document.getElementById("milisecond").innerText = "00";
+
+
+    let parent_start_stop = document.getElementsByClassName("start")[0];
+    let child_node = parent_start_stop.children[0];
+    // remove the current functionality from the start class
+    // fix to start button after all
+    parent_start_stop.removeChild(child_node);
+    parent_start_stop.appendChild(start_btn);
+})
+
+
+
+
+// add laps container
+let setLapIntervalFunc = null;
+let total_laps = 0;
+const laps_btn = document.getElementById("laps-btn");
+laps_btn.addEventListener("click", ()=>{
+    const laps_container = document.getElementsByClassName("laps-container")[0];
+    if(laps_container.childElementCount === 0)
+    {
+        const table = document.createElement("table");
+        const row = document.createElement("tr");
+        const th1 = document.createElement("th");th1.className = "laps-val";th1.innerText = "Laps";
+        const th2 = document.createElement("th");th2.className = "time-val";th2.innerText = "Times";
+        const th3 = document.createElement("th");th3.className = "total-time-val";th3.innerText = "Total time";
+        row.appendChild(th1);row.appendChild(th2);row.appendChild(th3);
+        table.appendChild(row);
+        laps_container.appendChild(table);
+    }
+    const table = laps_container.children[0];
+    const row = document.createElement("tr");
+    let last_lap = {
+        hr:0,
+        min:0,
+        sec:0,
+        ms:0,
+    }
+    setLapIntervalFunc = setInterval(()=>{
+        last_lap.ms += 1;
+        if(last_lap.ms == 100)
+        {
+            last_lap.ms = 0;
+            last_lap.sec += 1;
+            if(last_lap.sec == 60)
+            {
+                last_lap.sec = 0;
+                last_lap.min += 1;
+                if(last_lap.min == 60)
+                {
+                    last_lap.min = 0;
+                    last_lap.hr += 1;
+                }
+            }
+        }
+    },10)
+    total_laps += 1;
+    const th1 = document.createElement("th");th1.className = "laps-val"; th1.innerText = total_laps;
+
+    // create laps time column
+    const th2 = document.createElement("th");th1.className = "time-val";
+    th2.innerText = last_lap.hr < 10 ? "0" + last_lap.hr.toString() : last_lap.hr;
+    th2.innerText += ":";
+    th2.innerText += last_lap.min < 10 ? "0" + last_lap.min.toString() : last_lap.min;
+    th2.innerText += ":";
+    th2.innerText += last_lap.sec < 10 ? "0" + last_lap.sec.toString() : last_lap.sec;
+    th2.innerText += ".";
+    th2.innerText += last_lap.ms < 10 ? "0" + last_lap.ms.toString() : last_lap.ms;
+
+    // create total time column
+    const th3 = document.createElement("th");th1.className = "time-time-val";
+    th3.innerText = document.getElementById("hour").innerText + ":";
+    th3.innerText += document.getElementById("minute").innerText + ":";
+    th3.innerText += document.getElementById("second").innerText + ".";
+    th3.innerText += document.getElementById("milisecond").innerText;
+
+
+    row.appendChild(th1); row.appendChild(th2); row.appendChild(th3);
+    table.insert(row);
+
+})
+
 
 
 
---
+++ temp.js
@@ -0,0 +1,23 @@
+<div class="box hour">
+            <span class = "numBox">00</span>
+            <span class = "unit">hr</span>
+            <span class = "numBox m-0 p-0">:</span>
+            
+        </div>
+        <!-- <div class="box colon m-0 p-0"><span class = "numBox m-0 p-0">:</span></div> -->
+        <div class="box minute">
+            <span class = "numBox">00</span>
+            <span class = "unit">min</span>
+            <span class = "numBox">:</span>
+            
+        </div>
+        <!-- <div class="box colon m-0 p-0"><span class = "numBox">:</span></div> -->
+        <div class="box second">
+            <span class = "numBox">00</span>
+            <span class = "unit">sec</span>
+            <span class = "numBox">.</span>
+        </div>
+        <!-- <div class="box decimal m-0 p-0"><span class = "numBox">.</span></div> -->
+        <div class="box">
+            <span class = "numBox milisecond"><sub>00</sub></span>
+        </div>
